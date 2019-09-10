13013ad498430d926d5eebb3322f9090475b1e6a
Jonny Cornish <jmbcornish@gmail.com>
2019-09-09 22:22:10 -0400

    completed PyBank - the main change was trying to figure out how to export the data into a .txt file... figured out two different ways including a ~print method (i.e. txtfile.write and a csv.writer method

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,7 +28,13 @@ with open(budget_data_csv, 'r') as csvfile:
 
     # method to get the previous file row:
     pre_row = next(budget_file_reader)
+    
     #print(f"preline: {pre_row}")
+    
+    # # method below as alternative to calculate the mom change
+    # PL_change = 0
+    # previous_PL = 0
+    
     
     for row in budget_file_reader:
         # calculate the number of months in the analysis period
@@ -49,6 +55,10 @@ with open(budget_data_csv, 'r') as csvfile:
             greatest_decrease_date = row[0]
         pre_row = row
 
+        # or, using previous_PL method...
+        # PL_change = row[1] - previous_PL
+        # previousl_PL = row[1]
+
 avg_change_mom = tot_change_mom / total_months
 avg_change_mom = round(avg_change_mom, 2)
     
@@ -58,3 +68,26 @@ print("Total: " + "$" + str(net_PL))
 print("Average Change: " + "$" + str(avg_change_mom))
 print("Greatest Increase in Profits: " + greatest_increase_date + " ($" + str(greatest_increase) + ")")
 print("Greatest Decrease in Profits: " + greatest_decrease_date + " ($" + str(greatest_decrease) + ")")
+
+# # the below line is only relevant for method (1) of printing to the txt file:
+# summary_results = list(zip([total_months, net_PL, avg_change_mom, greatest_increase, greatest_increase_date, greatest_decrease, greatest_decrease_date]))
+# create the file to write to
+budget_data_txt_output = os.path.join('budget_data_txt.txt')
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(budget_data_txt_output, 'w', newline='') as txtfile:
+
+    # # Two ways to put this into the text file:
+    # # (1) Initialize txt.writer
+    # txtwriter = csv.writer(txtfile, delimiter=',')
+    # txtwriter.writerow(["Total Months", "Total", "Average Change", "Greatest Increase in Profits", "Greatest Increase Date",
+    # "Greatest Decrease in Profits", "Greatest Decrease"])
+    # txtwriter.writerow(summary_results)
+
+    # # (2) simply print everything you want into the text file
+    txtfile.write("Total Months: " + str(total_months) + "\n")
+    txtfile.write("Total: " + "$" + str(net_PL) + "\n")
+    txtfile.write("Average Change: " + "$" + str(avg_change_mom) + "\n")
+    txtfile.write("Greatest Increase in Profits: " + greatest_increase_date + " ($" + str(greatest_increase) + ")" + "\n")
+    txtfile.write("Greatest Decrease in Profits: " + greatest_decrease_date + " ($" + str(greatest_decrease) + ")" + "\n")
+
